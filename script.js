c2ea784d4386cd77efb046501b4c3a15ff3ceafa
Henrique Stolle <henriquestolleh@gmail.com>
2025-01-29 11:17:55 -0300

    fixed & add: problem with menu-burger fixed. smooth scrolling

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function toggleMenu() {
   const menu = document.getElementById("menu");
+  const menuIcon = document.querySelector(".menu-icon");
+
   menu.classList.toggle("show");
+  menuIcon.classList.toggle("active");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  });
+
+  elements.forEach((el) => observer.observe(el));
+});
